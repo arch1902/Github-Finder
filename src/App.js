b3bbc24a915857f907127df9a255b802b21a4dec
finvic <pranshu@Yogeshs-Air.domain.name>
2020-11-30 15:44:02 +0530

    user page added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/layout/Navbar'
 //import logo from './logo.svg';
 import './App.css';
 import Users from './components/users/Users'
+import User from './components/users/User'
 import Search from './components/users/Search'
 import axios from 'axios';
 import Alert from './components/layout/Alert'
@@ -14,8 +15,10 @@ class App extends Component {
 
   state ={
     users:[],
+    user:{},
     loading: false,
-    alert:null
+    alert:null,
+    repos:[]
   }
 
   // async componentDidMount(){
@@ -37,6 +40,22 @@ class App extends Component {
     this.setState({users:res.data.items ,loading : false});
   }
 
+  getUser = async username=>{
+    this.setState({loading:true});
+
+    const res = await axios.get(`https://api.github.com/users/${username}?&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({user :res.data ,loading : false});
+  }
+
+  getUserRepos = async username=>{
+    this.setState({loading:true});
+
+    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({repos :res.data ,loading : false});
+  }
+
   clearUsers = () =>{
     this.setState({users:[],loading:false});
   }
@@ -48,7 +67,7 @@ class App extends Component {
   }
 
   render(){
-    const {users,loading}=this.state;
+    const {users,user,loading,repos}=this.state;
 
 
     return (
@@ -70,6 +89,9 @@ class App extends Component {
               </Fragment>
             )}/>
             <Route exact path='/about' component={About}/>
+            <Route  exact path='/user/:login' render={props=>(
+              <User {...props} getUser={this.getUser} getUserRepos={this.getUserRepos} user={user} repos={repos} loading ={loading}/>
+            )}/>
           </Switch>
           
           
